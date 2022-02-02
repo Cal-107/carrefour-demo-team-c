6da26909e96b847f2cae1ff7c6272ce5043d6b78
gabriele-annese <gannese06@gmail.com>
2022-02-02 18:16:58 +0100

    add categories migration

--- public/js/app.js
+++ public/js/app.js
@@ -49931,8 +49931,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/calogerobonito/Desktop/Esercitazioni Boolean/carrefour-demo-team-c/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/calogerobonito/Desktop/Esercitazioni Boolean/carrefour-demo-team-c/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/gabri/Boolean/carrefour-demo-team-c/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/gabri/Boolean/carrefour-demo-team-c/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

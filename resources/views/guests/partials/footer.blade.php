ae803938b6d58472e50dcba7105b9f751f0401dd
Dario0122 <dario.di.cuia@gmail.com>
2022-02-07 15:55:23 +0100

    top section footer

---
+++ resources/views/guests/partials/footer.blade.php
@@ -0,0 +1,62 @@
+<footer class="d-flex justify-content-center">
+    <div class="service mt-4">
+        <h2 class="mb-4">Carrefour per te - I nostri servizi</h2>
+        <div class="top-section d-flex">
+            <div class="col-6 p-0 pr-2">
+                <div class="app pr-2 ">
+    
+                </div>
+            </div>
+            <div class="col-6 p-0 pl-2">
+                <div class="shipping pl-2">
+                    <ul class="">
+                        <li>
+                            <a href="#">
+                                <div class="card d-flex">
+                                    <img src="https://www.carrefour.it/on/demandware.static/-/Library-Sites-carrefour-library-IT/default/dw48cbac27/carrefour_now.svg" alt="">
+                                    <div class="text">
+                                        <h6><strong>Carrefour NOW</strong></h6>
+                                        <span>Il nuovo servizio di consegna veloce</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="card d-flex">
+                                    <img src="https://www.carrefour.it/on/demandware.static/-/Library-Sites-carrefour-library-IT/default/dw9a8aa761/franchising.svg" alt="">
+                                    <div class="text">
+                                        <h6><strong>Carrefour NOW</strong></h6>
+                                        <span>Il nuovo servizio di consegna veloce</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="card d-flex">
+                                    <img src="https://www.carrefour.it/on/demandware.static/-/Library-Sites-carrefour-library-IT/default/dw48cbac27/carrefour_now.svg" alt="">
+                                    <div class="text">
+                                        <h6><strong>Carrefour NOW</strong></h6>
+                                        <span>Il nuovo servizio di consegna veloce</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="card d-flex">
+                                    <img src="https://www.carrefour.it/on/demandware.static/-/Library-Sites-carrefour-library-IT/default/dw48cbac27/carrefour_now.svg" alt="">
+                                    <div class="text">
+                                        <h6><strong>Carrefour NOW</strong></h6>
+                                        <span>Il nuovo servizio di consegna veloce</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>

a8b2011a6c6dcd09601a885373317b6175716380
Bonu93 <luca.morucci.kb@gmail.com>
2022-02-07 19:02:31 +0100

    add create

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'brand', 
+        'description', 
+        'weight', 
+        'price_per_kg', 
+        'price', 
+        'slug'
+    ];
 }

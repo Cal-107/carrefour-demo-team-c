e494fbe58ce3e08e6e35ab101e28e7373943f105
dokkalemon <dottori.roberto@gmail.com>
2022-02-07 18:16:23 +0100

    create route and view

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::middleware('auth')
     ->group(function() {
         // admin hompage
         Route::get('/', 'HomeController@index')->name('home');
+        Route::resource('/categories', 'CategoryController');
     });
 
 

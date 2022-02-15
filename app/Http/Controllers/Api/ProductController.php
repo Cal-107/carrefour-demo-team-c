1494fd335993086e0255cf7703b5d4962f199fe1
Bonu93 <luca.morucci.kb@gmail.com>
2022-02-15 18:25:07 +0100

    fixed

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Product;
@@ -11,11 +12,13 @@ class ProductController extends Controller
     //product archive
     public function index() {
         // senza impaginazione
-        $products = Product::all()->with(['category']);
+        $products = Product::all();
+
+        $categories = Category::all();
 
         // $products = Product::orderBy('id', 'desc')->paginate(3);
         // dump($products);
 
-        return response()->json($products);
+        return response()->json([$products, $categories]);
     }
 }

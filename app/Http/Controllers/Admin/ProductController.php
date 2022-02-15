b2ffadeab57b769cbd4653ca37bd039d85b72c31
“Cal-107” <kaly___7@hotmail.it>
2022-02-15 15:43:49 +0100

    add seeder and fix

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -102,9 +102,9 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Product $product)
     {
-        //
+        return view('admin.products.edit', compact('product'));
     }
 
     /**
---
+++ config/products.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    [
+        'name' => 'Vallelata Fior di Latte Mozzarella Fresca 125 g',
+        'brand' => 'Vallelata',
+        'description' => 'Formaggi',
+        'weight' => '0.125',
+        'price_per_kg' => '10.80',
+        'price' => '1.35',
+        'image' => 'https://www.vallelata.it/wp-content/uploads/2014/06/    3D-Vallelata-mozzarella-200g-PULIAMO-IL-PARCO-2.png',
+        'slug' => 'vallelata-fior-di-latte-mozzarella-fresca-125-g'
+    ],
+    [
+        'name' => 'Galbani Santa Lucia Tris Mozzarella 3 x 125 g',
+        'brand' => 'Galbani',
+        'description' => 'Formaggi',
+        'weight' => '0.375',
+        'price_per_kg' => '9.84',
+        'price' => '3,69',
+        'image' => 'https://www.supermercato24.it/asset/smhd/5e346/a354a/bedf0/681494194964867968_img.jpg',
+        'slug' => 'galbani-santa-lucia-tris-mozzarella-3-x-125-g'
+    ],
+    [
+        'name' => 'Biraghi Gran Biraghi Formaggio Stagionato Grattugiato Fresco 100 g',
+        'brand' => 'Biraghi',
+        'description' => 'Formaggi',
+        'weight' => '0.100',
+        'price_per_kg' => '15.50',
+        'price' => '1.55',
+        'image' => 'https://www.carrefour.it/dw/image/v2/BDWP_PRD/on/demandware.static/-/Sites-carrefour-master-catalog-IT/default/dw26875fd9/large/GRATTUGIATOFRESCOGRANBIRAGHI-8002004217554-1.png?sw=260&sh=260&sm=fit&sfrm=png',
+        'slug' => 'biraghi-gran-biraghi-formaggio-stagionato-grattugiato-fresco-100-g'
+    ],
+    [
+        'name' => 'Aia aeQuilibrium Petto di Tacchino arrosto cotto al forno 140 g',
+        'brand' => 'Aia',
+        'description' => 'Salumi',
+        'weight' => '0.140',
+        'price_per_kg' => '13.50',
+        'price' => '1.89',
+        'image' => 'https://www.carrefour.it/dw/image/v2/BDWP_PRD/on/demandware.static/-/Sites-carrefour-master-catalog-IT/default/dwf9bb0804/large/PETTODITACCHINOALFORNO-8008110258634-1.png?sw=260&sh=260&sm=fit&sfrm=png',
+        'slug' => 'aia-aeQuilibrium-petto-di-tacchino-arrosto-cotto-al-forno-140-g'
+    ],
+    [
+        'name' => 'Negroni Cotto Stella Prosciutto Cotto Alta Qualità 110 g',
+        'brand' => 'Negroni',
+        'description' => 'Salumi',
+        'weight' => '0.110',
+        'price_per_kg' => '30.91',
+        'price' => '3.40',
+        'image' => 'https://www.carrefour.it/dw/image/v2/BDWP_PRD/on/demandware.static/-/Sites-carrefour-master-catalog-IT/default/dw6543170e/large/PROSCIUTTOCOTTOAFFETNEGRONI-8028257011497-1.png?sw=260&sh=260&sm=fit&sfrm=png',
+        'slug' => 'negroni-cotto-stella-prosciutto-cotto-alta-qualità-110-g'
+    ],
+];

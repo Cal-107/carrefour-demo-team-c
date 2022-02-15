103823fcce27c33a05f9ccf529a5275842a0e9fb
Bonu93 <luca.morucci.kb@gmail.com>
2022-02-15 17:36:36 +0100

    add db relation

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+
+    public function products() {
+        return $this->hasMany('App\Product');
+    }
     protected $fillable = [
         'category_name',
         'slug',

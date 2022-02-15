6da7759b34a04487dea3be737f6f848e4db037f3
“Cal-107” <kaly___7@hotmail.it>
2022-02-15 19:53:37 +0100

    add fix

--- resources/js/components/Banner.vue
+++ resources/js/components/Banner.vue
@@ -1,54 +1,64 @@
 <template>
-  <section class="mt-5">
-      <!-- hero image -->
-      <div>
-          <div class=" container-fluid heroContainer">
-              <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwa3efb334/banner/suggestion-banner/carrefour-club-novita/NOVITA_CRF_CLUB_sconto_pdv_ecom_Inner_D.jpg" alt="">
-          </div>
-      </div>
+    <section class="mt-5">
+        <!-- hero image -->
+        <div>
+            <div class="container-fluid heroContainer">
+                <img
+                    src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwa3efb334/banner/suggestion-banner/carrefour-club-novita/NOVITA_CRF_CLUB_sconto_pdv_ecom_Inner_D.jpg"
+                    alt=""
+                />
+            </div>
+        </div>
 
-      <!-- promo banner -->
-      <div class="mt-5">
-          <div class="container PromoContainer">
-              <div class="row">
-                  <div class="col-6">
-                      <img class="mb-2" src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw79599c54/banner/main-carousel/food/placeholder/carrefour-bio/Bannerpiccolo_BIO_Placheholder_mob.jpg" alt="carrefour-promo">
-                      <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwc4c591fc/banner/main-carousel/food/banner_piccolo/capitan-findus/HOMEPAGE_MOBILE_768X200_2.jpg" alt="carrefour-promo">
-                  </div>
-                  
-                  <div class="col-6">
-                      <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwc815521a/banner/main-carousel/food/placeholder/terre-d-italia/Bannergrande_mob_02.jpg" alt="">
-                  </div>
-              </div>
-          </div>
+        <!-- promo banner -->
+        <div class="mt-5">
+            <div class="container PromoContainer">
+                <div class="row">
+                    <div class="col-6">
+                        <img
+                            class="mb-2"
+                            src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw79599c54/banner/main-carousel/food/placeholder/carrefour-bio/Bannerpiccolo_BIO_Placheholder_mob.jpg"
+                            alt="carrefour-promo"
+                        />
+                        <img
+                            src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwc4c591fc/banner/main-carousel/food/banner_piccolo/capitan-findus/HOMEPAGE_MOBILE_768X200_2.jpg"
+                            alt="carrefour-promo"
+                        />
+                    </div>
 
-      </div>
-  </section>
+                    <div class="col-6">
+                        <img
+                            src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwc815521a/banner/main-carousel/food/placeholder/terre-d-italia/Bannergrande_mob_02.jpg"
+                            alt=""
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
 export default {
-    name: 'Banner',
-}
+    name: "Banner",
+};
 </script>
 
 <style lang="scss" scoped>
-
-section{
-    .heroContainer{
+section {
+    .heroContainer {
         text-align: center;
 
-        img{
+        img {
             width: 80%;
         }
     }
 
-    .PromoContainer{
-        img{
+    .PromoContainer {
+        img {
             width: 100%;
             border-radius: 15px;
         }
     }
 }
-
 </style>
--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,96 +1,112 @@
 <template>
-<header>
-     <!-- headerBunner  -->
-    <div class="headerBunner d-flex justify-content-between align-items-center p-1 px-4">
-        <div class="HeaderBunnerLeft">
-            <a href="/">Lavora con noi</a>
-        </div>
-        <div class="HeaderBunnerRight ">
-            <ul class="d-flex align-items-center">
-                <li>
-                    <a href="/">Serve aiuto?</a>
-                </li>
-                <li>
-                    <a href="/">Volantini</a>
-                </li>
-                <li>
-                    <a href="/">Punti vendita</a>
-                </li>
-                <li>
-                    <a href="/">Servizi</a>
-                </li>
-                <li>
-                    <a href="/">Carta Carrefour Findomestic</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-
-
-    <!-- header-top -->
-    <div class="d-flex pt-4 pb-2 px-3 mx-3 justify-content-between align-items-center">
-        <div class="header-logo ">
-            <a href="/" class="d-flex">
-                <img class="img-fluid" src="https://cdn.worldvectorlogo.com/logos/carrefour-supermarket-logo.svg" alt="">
-            </a>
-        </div>
-        <!-- search-bar -->
-        <div class="search-bar-container mx-2">
-            <form class="searchForm">
-                 <input class="SearchBar Search" id="search" type="text" placeholder="Di cosa hai bisogno?" required>
-                 <input class="SearchBar" id="submit" type="submit" value="Search">
-            </form>
-        </div>
-
-        <!-- header-right -->
-        <div class="right-header d-flex align-items-center">
-            <!--badge  -->
-             <div class="paybackBadge mx-5">
-                 <a href="/">
-                    <div class="payback-top">PAYBACK</div>
-                    <div class="payback-bottom">Scopri Payback</div>
-                 </a>
+    <header>
+        <!-- headerBunner  -->
+        <div
+            class="headerBunner d-flex justify-content-between align-items-center p-1 px-4"
+        >
+            <div class="HeaderBunnerLeft">
+                <a href="/">Lavora con noi</a>
             </div>
-            <!-- carrello -->
-            <div class="minicart mx-4">
-                <a href="/">
-                      <i class="fas fa-shopping-cart"></i>
+            <div class="HeaderBunnerRight">
+                <ul class="d-flex align-items-center">
+                    <li>
+                        <a href="/">Serve aiuto?</a>
+                    </li>
+                    <li>
+                        <a href="/">Volantini</a>
+                    </li>
+                    <li>
+                        <a href="/">Punti vendita</a>
+                    </li>
+                    <li>
+                        <a href="/">Servizi</a>
+                    </li>
+                    <li>
+                        <a href="/">Carta Carrefour Findomestic</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- header-top -->
+        <div
+            class="d-flex pt-4 pb-2 px-3 mx-3 justify-content-between align-items-center"
+        >
+            <div class="header-logo">
+                <a href="/" class="d-flex">
+                    <img
+                        class="img-fluid"
+                        src="https://cdn.worldvectorlogo.com/logos/carrefour-supermarket-logo.svg"
+                        alt=""
+                    />
                 </a>
             </div>
+            <!-- search-bar -->
+            <div class="search-bar-container mx-2">
+                <form class="searchForm">
+                    <input
+                        class="SearchBar Search"
+                        id="search"
+                        type="text"
+                        placeholder="Di cosa hai bisogno?"
+                        required
+                    />
+                    <input
+                        class="SearchBar"
+                        id="submit"
+                        type="submit"
+                        value="Search"
+                    />
+                </form>
+            </div>
+
+            <!-- header-right -->
+            <div class="right-header d-flex align-items-center">
+                <!--badge  -->
+                <div class="paybackBadge mx-5">
+                    <a href="/">
+                        <div class="payback-top">PAYBACK</div>
+                        <div class="payback-bottom">Scopri Payback</div>
+                    </a>
+                </div>
+                <!-- carrello -->
+                <div class="minicart mx-4">
+                    <a href="/">
+                        <i class="fas fa-shopping-cart"></i>
+                    </a>
+                </div>
+            </div>
         </div>
-    </div>
-    
-    <!-- header-bottom -->
 
-    <div class="p-2 mx-2">
-        <div class="headerNavRight">
-            <ul class="d-flex align-items-center">
-                <li>
-                    <router-link class="link-header" :to="{name: 'home'}">Spesa Online</router-link>
-                    
-                </li>
-                <li>
-                    <a href="\">Promozioni</a>
-                </li>
-            </ul>
+        <!-- header-bottom -->
+
+        <div class="p-2 mx-2">
+            <div class="headerNavRight">
+                <ul class="d-flex align-items-center">
+                    <li>
+                        <router-link class="link-header" :to="{ name: 'home' }"
+                            >Spesa Online</router-link
+                        >
+                    </li>
+                    <li>
+                        <a href="\">Promozioni</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</header>
-
-
+    </header>
 </template>
 
 <script>
 export default {
-    name: 'Header',
-}
+    name: "Header",
+};
 </script>
 
 <style lang="scss" scoped>
-
-header{
-
-    a{
+header {
+    border-bottom: 1px solid rgba(206, 206, 206, 0.523);
+    a {
         color: black;
         text-decoration: none;
         text-decoration: none;
@@ -101,12 +117,12 @@ header{
         }
     }
 
-    ul{
+    ul {
         list-style: none;
         margin: 0;
     }
 
-    .headerBunner{
+    .headerBunner {
         background-color: #1b3d79;
         color: white;
         font-size: 14px;
@@ -114,24 +130,22 @@ header{
         a {
             color: white;
 
-            &:hover{
+            &:hover {
                 color: #0970e6;
             }
         }
 
-        .HeaderBunnerRight{
-            ul{
-
-
-                li{
+        .HeaderBunnerRight {
+            ul {
+                li {
                     margin: 0 10px;
 
-                    a{
+                    a {
                         padding-right: 20px;
                         border-right: 1px solid white;
                         transition: color 0.4s;
 
-                        &:hover{
+                        &:hover {
                             color: #0970e6;
                         }
                     }
@@ -140,55 +154,51 @@ header{
         }
     }
 
-    
-    
-    .header-logo{
-        a{
-            img{
+    .header-logo {
+        a {
+            img {
                 width: 200px;
             }
         }
     }
 
-    .search-bar-container{
+    .search-bar-container {
         max-width: 836px;
         width: 50%;
 
-        .searchForm{
+        .searchForm {
             max-width: 100%;
 
-            .Search{
+            .Search {
                 width: 80%;
             }
 
-            .SearchBar{
+            .SearchBar {
                 padding: 7px 10px 7px 10px;
                 color: #929292;
                 border: 1px solid #d2d2d2;
                 border-radius: 50px;
-                
             }
         }
     }
 
-
-    .paybackBadge{
+    .paybackBadge {
         line-height: 1;
 
-        .payback-top{
+        .payback-top {
             font-size: 12px;
             color: #4c6696;
         }
-        .payback-bottom{
+        .payback-bottom {
             color: #0970e6;
             font-size: 18px;
             font-weight: bold;
         }
     }
 
-    .minicart{
-        a{
-            i{
+    .minicart {
+        a {
+            i {
                 font-size: 25px;
                 margin-right: 10px;
                 color: #0970e6;
@@ -196,23 +206,19 @@ header{
         }
     }
 
-
-    .headerNavRight{
+    .headerNavRight {
         font-size: 17px;
 
-
-        li{       
-            margin-right: 25px;   
-            .link-header{
+        li {
+            margin-right: 25px;
+            .link-header {
                 color: #0e3368;
                 transition: color 0.4s;
-                &:hover{
+                &:hover {
                     color: #0970e6;
-            }
-
+                }
             }
         }
-    }       
+    }
 }
-
 </style>
